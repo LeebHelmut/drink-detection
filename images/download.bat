596007f40f21243d821341dcbfab18eadd29a358
deder <d.eder@outlook.at>
2019-10-30 12:02:47 +0100

    added images

---
+++ images/download.bat
@@ -0,0 +1,7 @@
+pip install google_images_download
+
+googleimagesdownload --keywords "Beer, Bier" --limit 100
+googleimagesdownload --keywords "Wine, Wein" --limit 100
+googleimagesdownload --keywords "Water, Wasser" --limit 100
+googleimagesdownload --keywords "Lemonade, Limonade" --limit 100
+googleimagesdownload --keywords "Cocktail" --limit 100
