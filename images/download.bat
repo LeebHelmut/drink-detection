7025b214d51030b496669a20190a19132347f957
deder <d.eder@outlook.at>
2019-11-06 09:26:03 +0100

    sorted out images; changed nr of steps

--- images/download.bat
+++ images/download.bat
@@ -1,7 +1,8 @@
 pip install google_images_download
 
-googleimagesdownload --keywords "Beer, Bier" --limit 100
-googleimagesdownload --keywords "Wine, Wein" --limit 100
-googleimagesdownload --keywords "Water, Wasser" --limit 100
-googleimagesdownload --keywords "Lemonade, Limonade" --limit 100
-googleimagesdownload --keywords "Cocktail" --limit 100
+googleimagesdownload --keywords "Beer Glass, Bier Glas" --limit 100
+googleimagesdownload --keywords "Wine Glass, Wein Glas" --limit 100
+googleimagesdownload --keywords "Water Glass, Wasser Glas" --limit 100
+# googleimagesdownload --keywords "Lemonade, Limonade" --limit 100
+googleimagesdownload --keywords "Cocktail Glass" --limit 100
+pause
